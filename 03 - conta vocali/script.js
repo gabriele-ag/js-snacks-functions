b7686ff0925516fa53b08fa1f3214d60ee3d99ba
gabriele-ag <gabrieleardizzone.gdesigner@gmail.com>
2025-04-26 16:57:06 +0200

    feat: end exer 3

--- 03 - conta vocali/script.js
+++ 03 - conta vocali/script.js
@@ -4,10 +4,21 @@ const word = 'javascript';
 
 
 // Dichiara la funzione qui.
+function numVocals(word) {
+    let vocals = 0
+    for (let i = 0; i < word.length; i++) {
+        if (word[i] === "a" || word[i] === "e" || word[i] === "i" || word[i] === "o" || word[i] === "u" ) {
+            vocals++
+        }
+    }
+    return vocals
+}
+
 
 
 // Invoca la funzione qui e stampa il risultato in console
-
+const result = numVocals(word)
+console.log(result)
 
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
