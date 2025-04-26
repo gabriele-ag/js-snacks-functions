7e98e01238ce94aac12394d560a772f51872a8fa
gabriele-ag <gabrieleardizzone.gdesigner@gmail.com>
2025-04-26 15:59:56 +0200

    feat: first exer done

---
+++ 01 - Iniziali/script.js
@@ -0,0 +1,27 @@
+/* Scrivi una funzione che accetti un array di nomi e restituisca nuovo un array con le iniziali di ogni parola dell'array fornito */
+
+const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
+
+
+// Dichiara la funzione qui.
+
+function isCapital(name) {
+    const firstLetter = []
+    for (let i = 0; i < names.length; i++ ) {
+    const name = names[i]
+        for (let j = 0; j < name.length; j++) {
+            const capital = name[j]
+            if (capital >= "A" && capital <= "Z") {
+                firstLetter.push(capital)
+            }
+        }
+        
+    }
+    return firstLetter
+}
+
+
+// Invoca la funzione qui e stampa il risultato in console
+console.log(isCapital(names))
+
+//Risultato atteso: ["A", "L", "M", "A", "G", "A"]
