97d2c1d47e7af4f56d57bca279eb6a84bc783cc3
gabriele-ag <gabrieleardizzone.gdesigner@gmail.com>
2025-04-26 17:30:37 +0200

    feat: exer 4 done

--- 04 - saluti/script.js
+++ 04 - saluti/script.js
@@ -4,10 +4,17 @@ const name = 'Mario';
 
 
 // Dichiara la funzione qui.
+function sayHello(name) {
+    const saluto = "Ciao" + " ";
+    const salutoUtente = saluto + name;
+    return salutoUtente;
+    
+}
 
 
 // Invoca la funzione qui e stampa il risultato in console
-
+const result = sayHello(name)
+console.log(result)
 
 
 //Risultato atteso se si passa 'Mario': // ciao Mario
